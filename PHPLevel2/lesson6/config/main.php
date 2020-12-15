edb2161435c1c44a47c73ca1ed20fcc3d41e06c7
Vyacheslav Ovchinnikov <vovchinnikov@plesk.com>
2020-12-15 17:22:32 +0700

    Lesson 6

---
+++ PHPLevel2/lesson6/config/main.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'db' => [
+        'dsn' => 'mysql:dbname=dz;host=127.0.0.1',
+        'user' => 'root',
+        'pwd' => '1531',
+    ],
+    'templates' => __DIR__ . '/../templates',
+];
